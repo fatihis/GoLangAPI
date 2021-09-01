d7b95d49e3834b73e74ca578c2675cdaa2e8e507
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-01 12:22:04 +0300

    Add DBUtils, utility for connection and colleciton

---
+++ DBUtils.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
+
+//UTILS
+func GetMongoDbConnection() (*mongo.Client, error) {
+
+	clientOptions := options.Client().
+		ApplyURI("mongodb+srv://fatih:1234@cluster0.jk26p.mongodb.net/fillusers?retryWrites=true&w=majority")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client, nil
+}
+
+func getMongoDbCollection(DbName string, CollectionName string) (*mongo.Collection, error) {
+	client, err := GetMongoDbConnection()
+
+	if err != nil {
+		return nil, err
+	}
+
+	collection := client.Database(DbName).Collection(CollectionName)
+	return collection, nil
+}
