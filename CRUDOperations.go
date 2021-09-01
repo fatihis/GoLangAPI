0ae317ac532bd9868cb8967d6b3574cf42bdb700
fatihis99@hotmail.com <isk.fatih1@gmail.com>
2021-09-01 12:23:03 +0300

    Add CRUDOperations, rest api functions

---
+++ CRUDOperations.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gofiber/fiber"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//REST API FUNCTIONS
+func getPerson(c *fiber.Ctx) {
+	collection, err := getMongoDbCollection("fillusers", "users")
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	var filter bson.M = bson.M{}
+
+	if c.Params("id") != "" {
+		c.Status(400)
+		return
+	}
+
+	id := c.Params("id")
+	objID, _ := primitive.ObjectIDFromHex(id)
+	filter = bson.M{"_id": objID}
+
+
+	var results []bson.M
+	cur, err := collection.Find(context.Background(), filter)
+	defer cur.Close(context.Background())
+
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	cur.All(context.Background(), &results)
+
+	if results == nil {
+		c.SendStatus(404)
+		return
+	}
+
+	json, _ := json.Marshal(results)
+	c.Send(json)
+}
+func getAllPersons(c *fiber.Ctx) {
+	collection, err := getMongoDbCollection("fillusers", "users")
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	var results []bson.M
+	cur, err := collection.Find(context.Background(), bson.M{})
+	defer cur.Close(context.Background())
+
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	cur.All(context.Background(), &results)
+
+	if results == nil {
+		c.SendStatus(404)
+		return
+	}
+
+	json, _ := json.Marshal(results)
+	c.Send(json)
+}
+
+func createPerson(c *fiber.Ctx) {
+	collection, err := getMongoDbCollection("fillusers", "users")
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	var person Person
+	json.Unmarshal([]byte(c.Body()), &person)
+
+	res, err := collection.InsertOne(context.Background(), person)
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	response, _ := json.Marshal(res)
+	c.Send(response)
+}
+func updatePerson(c *fiber.Ctx) {
+	collection, err := getMongoDbCollection("fillusers", "users")
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+	var person Person
+	json.Unmarshal([]byte(c.Body()), &person)
+
+	update := bson.M{
+		"$set": person,
+	}
+
+
+	objID, _ := primitive.ObjectIDFromHex(c.Params("id"))
+	res, err := collection.UpdateOne(context.Background(), bson.M{"_id": objID}, update)
+
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	response, _ := json.Marshal(res)
+	c.Send(response)
+}
+func deletePerson(c *fiber.Ctx) {
+
+	collection, err := getMongoDbCollection("fillusers", "users")
+
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	objID, _ := primitive.ObjectIDFromHex(c.Params("id"))
+	res, err := collection.DeleteOne(context.Background(), bson.M{"_id": objID})
+
+	if err != nil {
+		c.Status(500).Send(err)
+		return
+	}
+
+	jsonResponse, _ := json.Marshal(res)
+	c.Send(jsonResponse)
+}
+
+
+
+
